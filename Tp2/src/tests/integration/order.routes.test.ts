2b4969865045fd03d441f00ba473477583a1a507
Santiago Recari <santi.recari.sr@gmail.com>
2025-11-18 17:45:16 -0300

    Arreglo test de integracion

--- Tp2/src/tests/integration/order.routes.test.ts
+++ Tp2/src/tests/integration/order.routes.test.ts
@@ -1,8 +1,7 @@
 import request from 'supertest';
 import makeapp from '../../app';
 import { orderService } from '../../services/order.service';
-import { test, expect, describe, beforeAll } from 'vitest';
-import { dir } from 'console';
+import { test, expect, describe, beforeEach } from 'vitest';
 
 const app = makeapp();
 
@@ -12,9 +11,9 @@ describe("Order Routes", () => {
         topping: ["jamon", "queso"],
         size: "M",
         status: "PENDING",
-        price: 100,
-        adress: "Zapiola 100"
+        address: "Zapiola 100"
     };
+
 
     test("POST /orders - create order", async () => {
         const response = await request(app).post("/orders").send(newOrder);
@@ -23,13 +22,20 @@ describe("Order Routes", () => {
         expect(response.body.topping).toEqual(newOrder.topping);
         expect(response.body.size).toBe(newOrder.size);
         expect(response.body.status).toBe(newOrder.status);
-        expect(response.body.price).toBe(newOrder.price);
+        expect(response.body.price).toBe(190); 
+        expect(response.body.address).toBe(newOrder.address);
     });
 
     test("GET /orders/:id - get orders by id", async () => {
         const response = await request(app).get("/orders/1");
         expect(response.status).toBe(200);
         expect(response.body).toHaveProperty("id");
+        expect(response.body.id).toBe(1);
+        expect(response.body.topping).toEqual(newOrder.topping);
+        expect(response.body.size).toBe(newOrder.size);
+        expect(response.body.status).toBe(newOrder.status);
+        expect(response.body.price).toBe(190);
+        expect(response.body.address).toBe(newOrder.address);
     });
 
 });
